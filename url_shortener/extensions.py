219bda1a1464d4eac48e21cb863fca6869a70ece
Alexandr <shurick672@gmail.com>
2020-05-13 22:14:41 +0300

    realy?

--- url_shortener/extensions.py
+++ url_shortener/extensions.py
@@ -1,5 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
 from flask_migrate import Migrate
 
 db = SQLAlchemy()
 migrate = Migrate()
+manager = Manager()
--- url_shortener/models.py
+++ url_shortener/models.py
@@ -9,6 +9,7 @@ class Link(db.Model):
     original_url = db.Column(db.String(512))
     short_url = db.Column(db.String(5))
     visits = db.Column(db.Integer, default=0)
+    test = db.Column(db.Integer)
     date_created = db.Column(db.DateTime, default=datetime.now)
 
     def __init__(self, **kwargs):
