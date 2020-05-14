e0f8e5f1b5e68c3b919e377bb59c0b97379d80a3
Alexandr <shurick672@gmail.com>
2020-05-14 11:32:03 +0300

    q

--- url_shortener/models.py
+++ url_shortener/models.py
@@ -8,7 +8,6 @@ class Link(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     original_url = db.Column(db.String(512))
     short_url = db.Column(db.String(5))
-    test = db.Column(db.String(5))
     visits = db.Column(db.Integer, default=0)
     date_created = db.Column(db.DateTime, default=datetime.now)
 
