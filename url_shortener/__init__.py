85745b1a9b1f3cb1f36eb3486815c5752c782b68
Alexandr <shurick672@gmail.com>
2020-05-14 09:55:46 +0300

    test

--- url_shortener/__init__.py
+++ url_shortener/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
-
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
 from .extensions import db
 from .routes import short
 from .models import Link
@@ -9,11 +10,18 @@ def create_app(config_file='settings.py'):
     app = Flask(__name__)
 
     app.config.from_pyfile(config_file)
-
-    db.init_app(app)
     
-
+    db.init_app(app)
     app.register_blueprint(short)
+    migrate = Migrate(app, db)
+    manager = Manager(app)
+    manager.add_command('db', MigrateCommand)
+    
+    
+    
 
 
     return app
+
+if __name__ == "__main__":
+    Manager(Flask(__name__)).run()
